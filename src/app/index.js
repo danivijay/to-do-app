5a8d21850f1a98335e3a248ff2dda5a51e103a21
Dani Vijay <danimvijay@gmail.com>
2018-04-06 22:24:35 +0530

    add component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,16 @@
-alert('it works');
+var React = require('react')
+var ReactDOM = require('react-dom')
+var createReactClass = require('create-react-class');
+
+class TodoComponent extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Hello</h1>
+        <p>World</p>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
